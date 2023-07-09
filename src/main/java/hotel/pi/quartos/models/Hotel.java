b526f61ad6d7aab7fb8bc9c8dfa4d3e1dcaf10e2
lucasrafael28 <r.lucas@escolar.ifrn.edu.br>
2023-07-09 16:58:48 -0300

    Configurando classe de modelo Evento

--- src/main/java/hotel/pi/quartos/models/Hotel.java
+++ src/main/java/hotel/pi/quartos/models/Hotel.java
@@ -1,10 +1,27 @@
 package hotel.pi.quartos.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Hotel {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nomeHotel;
 	private String cidadeHotel;
 	private String gerenteHotel;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getNomeHotel() {
 		return nomeHotel;
