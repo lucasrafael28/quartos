b864a767c0796878efd28fc46eece0652b58d0f1
lucasrafael28 <r.lucas@escolar.ifrn.edu.br>
2023-07-09 16:01:49 -0300

    Modelo de Hotel criado

---
+++ src/main/java/hotel/pi/quartos/models/Hotel.java
@@ -0,0 +1,32 @@
+package hotel.pi.quartos.models;
+
+public class Hotel {
+
+	private String nomeHotel;
+	private String cidadeHotel;
+	private String gerenteHotel;
+
+	public String getNomeHotel() {
+		return nomeHotel;
+	}
+
+	public void setNomeHotel(String nomeHotel) {
+		this.nomeHotel = nomeHotel;
+	}
+
+	public String getCidadeHotel() {
+		return cidadeHotel;
+	}
+
+	public void setCidadeHotel(String cidadeHotel) {
+		this.cidadeHotel = cidadeHotel;
+	}
+
+	public String getGerenteHotel() {
+		return gerenteHotel;
+	}
+
+	public void setGerenteHotel(String gerenteHotel) {
+		this.gerenteHotel = gerenteHotel;
+	}
+}
