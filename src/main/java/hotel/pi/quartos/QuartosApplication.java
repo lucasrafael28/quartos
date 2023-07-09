344a8ea3953387b7ea0847110a27274d16baa361
lucasrafael28 <r.lucas@escolar.ifrn.edu.br>
2023-07-09 14:23:10 -0300

    Arquivos adicionais

---
+++ src/main/java/hotel/pi/quartos/QuartosApplication.java
@@ -0,0 +1,13 @@
+package hotel.pi.quartos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hotel/pi/quartos/QuartosApplicationTests.java
@@ -0,0 +1,13 @@
+package hotel.pi.quartos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
