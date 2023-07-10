00b1159980cffc4d1e833b8cbd834cbe6af2c395
lucasrafael28 <r.lucas@escolar.ifrn.edu.br>
2023-07-09 21:29:48 -0300

    Criando a listagem de eventos

--- src/main/java/hotel/pi/quartos/controllers/HoteisController.java
+++ src/main/java/hotel/pi/quartos/controllers/HoteisController.java
@@ -1,30 +1,43 @@
 package hotel.pi.quartos.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import hotel.pi.quartos.models.Hotel;
 import hotel.pi.quartos.repositories.HotelRepository;
 
 @Controller
+@RequestMapping("/hoteis")
 public class HoteisController {
 
 	@Autowired
 	HotelRepository hr;
-	
-	@RequestMapping("/hoteis/form")
+
+	@GetMapping("/form")
 	public String form() {
 		return "hoteis/formHotel";
 	}
-	
-	@PostMapping("/hoteis")
+
+	@PostMapping
 	public String adicionar(Hotel hotel) {
 
 		System.out.println(hotel);
 		hr.save(hotel);
 
 		return "hoteis/hotel-adicionado";
+	}
+
+	@GetMapping
+	public ModelAndView listar() {
+		List<Hotel> hoteis = hr.findAll();
+		ModelAndView mv = new ModelAndView("hoteis/lista");
+		mv.addObject("hoteis", hoteis);
+		return mv;
 	}
 }
