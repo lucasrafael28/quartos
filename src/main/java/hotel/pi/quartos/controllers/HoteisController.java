31a0236cd68366e38ae616733ebaa8585078b224
lucasrafael28 <r.lucas@escolar.ifrn.edu.br>
2023-07-11 15:57:07 -0300

    Criando página de detalhes do evento

--- src/main/java/hotel/pi/quartos/controllers/HoteisController.java
+++ src/main/java/hotel/pi/quartos/controllers/HoteisController.java
@@ -1,10 +1,12 @@
 package hotel.pi.quartos.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -39,5 +41,22 @@ public class HoteisController {
 		ModelAndView mv = new ModelAndView("hoteis/lista");
 		mv.addObject("hoteis", hoteis);
 		return mv;
+	}
+
+	@GetMapping("/{id}")
+	public ModelAndView detalhar(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Hotel> opt = hr.findById(id);
+
+		if (opt.isEmpty()) {
+			md.setViewName("redirect:/hoteis");
+			return md;
+		}
+
+		md.setViewName("hoteis/detalhes");
+		Hotel hotel = opt.get();
+		md.addObject("hotel", hotel);
+
+		return md;
 	}
 }
